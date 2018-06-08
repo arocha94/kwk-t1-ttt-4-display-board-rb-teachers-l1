cb7835334cec8e8b2258aca732f494d17408654b
Alejandra Rocha <arocha94@gmail.com>
2018-06-08 14:28:16 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,7 @@
 # out the current state.
 
 def display_board(board)
-  puts "#{board}"
+  board = board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
   puts row = ["   " "|" "   " "|" "   "]
   puts separator = "-----------"
   puts row 
